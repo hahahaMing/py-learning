8e4809dd3ab95e6f54f8c39d1fdadafa366eea84
nb ub ming <eiyaming@163.com>
2019-10-22 18:10:13 +0800

    change the fizzbuzz code i can see its faster performance

--- pytorch_demo/v03_pytorch_08.py
+++ pytorch_demo/v03_pytorch_08.py
@@ -3,7 +3,11 @@
 """
 import numpy as np
 import torch
+import time
+import datetime
 
+USE_CUDA = torch.cuda.is_available()
+# USE_CUDA = False
 NUM_DIGITS = 10
 # One-hot encode the desired outputs: [number, "fizz", "buzz", "fizzbuzz"]
 def fizz_buzz_encode(i):
@@ -18,7 +22,7 @@ def fizz_buzz_encode(i):
 
 
 def fizz_buzz_decode(i, prediction):
-    return [str(i), "fizz", "buzz", "fizzbuzz"][prediction]
+    return [str(i+1), "fizz", "buzz", "fizzbuzz"][prediction]
 
 # Represent each input by an array of its binary digits.
 def binary_encode(i, num_digits):
@@ -28,27 +32,29 @@ trX = torch.Tensor([binary_encode(i, NUM_DIGITS) for i in range(101, 2 ** NUM_DI
 trY = torch.LongTensor([fizz_buzz_encode(i) for i in range(101, 2 ** NUM_DIGITS)])
 
 # Define the model
-NUM_HIDDEN = 100
+NUM_HIDDEN = 1000
 model = torch.nn.Sequential(
     torch.nn.Linear(NUM_DIGITS, NUM_HIDDEN),
     torch.nn.ReLU(),
     torch.nn.Linear(NUM_HIDDEN, 4)
 )
-if torch.cuda.is_available():
+if USE_CUDA:
     model = model.cuda()
 
 loss_fn = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr = 0.05)
 
+# start = time.clock()
+starttime = datetime.datetime.now()
 # Start training it
-BATCH_SIZE = 128
+BATCH_SIZE = 512
 for epoch in range(10000):
     for start in range(0, len(trX), BATCH_SIZE):
         end = start + BATCH_SIZE
         batchX = trX[start:end]
         batchY = trY[start:end]
 
-        if torch.cuda.is_available():
+        if USE_CUDA:
             batchX = batchX.cuda()
             batchY = batchY.cuda()
 
@@ -60,12 +66,22 @@ for epoch in range(10000):
         loss.backward()
         optimizer.step()
 
-    # # Find loss on training data
-    # loss = loss_fn(model(trX), trY)
-    # print('Epoch:', epoch, 'Loss:', loss)
+# elapsed = (time.clock() - start)
+# print("Time used:",elapsed)
+endtime = datetime.datetime.now()
+
+seconds = (endtime - starttime).seconds
+start = starttime.strftime('%Y-%m-%d %H:%M')
+# 100 秒
+# 分钟
+minutes = seconds // 60
+second = seconds % 60
+print((endtime - starttime))
+timeStr = str(minutes) + '分钟' + str(second) + "秒"
+print("程序从 " + start + ' 开始运行,运行时间为：' + timeStr)
 
 testX = torch.Tensor([binary_encode(i,NUM_DIGITS)for i in range(1,101)])
-if torch.cuda.is_available():
+if USE_CUDA:
     testX = testX.cuda()
 with torch.no_grad():
     testY = model(testX)
